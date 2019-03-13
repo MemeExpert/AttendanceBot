e483a13dbf3ba62462a1f45cbd9e0f1f90c1d60f
Steven Y <stevenyildirim@gmail.com>
2019-03-13 10:33:22 -0400

    Initial discord bot commit

---
+++ discordBot.py
@@ -0,0 +1,35 @@
+import discord
+import logging
+import config
+
+
+# This is for logging into a discord.log file
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+
+TOKEN = config.discordToken # Needs to be inside config.py 
+
+client = discord.Client() # Init the bot
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(TOKEN)
