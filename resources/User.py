abe6d11eddab594f84fbe6dca600542bd6824b9a
myran2 <henrytgordon@gmail.com>
2019-03-24 21:44:52 -0400

    Add PUT and DEL handlers for users

--- resources/User.py
+++ resources/User.py
@@ -19,7 +19,9 @@ class UserResource(Resource):
             return {'message': 'No input data provided'}, 400
 
         # Validate and deserialize input
-        data = user_schema.load(json_data, partial=False)
+        data = user_schema.load(json_data)
+        # if errors:
+        #    return errors, 422
         user = User.query.filter_by(discordName=data['discordName']).first()
         if user:
             return {'message': 'User already exists'}, 400
@@ -34,3 +36,39 @@ class UserResource(Resource):
 
         result = user_schema.dump(user)
         return {"status": 'success', 'data': result}, 201
+
+    def put(self):
+        json_data = request.get_json(force=True)
+        if not json_data:
+            return {'message': 'No input data provided'}, 400
+        # Validate and deserialize input
+        data = user_schema.load(json_data)
+        # if errors:
+        #    return errors, 422
+        user = User.query.filter_by(id=data['id']).first()
+        if not user:
+            return {'message': 'User does not exist'}, 400
+
+        user.displayName = data['displayName'],
+        user.discordName = data['discordName'],
+        user.slackName = data['slackName']
+
+        db.session.commit()
+
+        result = user_schema.dump(user)
+        return {"status": 'success', 'data': result}, 204
+
+    def delete(self):
+        json_data = request.get_json(force=True)
+        if not json_data:
+            return {'message': 'No input data provided'}, 400
+
+        # Validate and deserialize input
+        data = user_schema.load(json_data)
+        # if errors:
+        #    return errors, 422
+        user = User.query.filter_by(id=data['id']).delete()
+        db.session.commit()
+
+        result = user_schema.dump(user)
+        return {"status": 'success', 'data': result}, 204
