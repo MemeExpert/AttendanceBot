600f8e6b86bf45a436818d4b419fab76dc1918ef
myran2 <henrytgordon@gmail.com>
2019-04-20 20:31:23 -0400

    allow event search by title

--- resources/Event.py
+++ resources/Event.py
@@ -18,6 +18,9 @@ class EventResource(Resource):
         if 'creator_id' in request.args:
             query = query.filter_by(creator_id=request.args['creator_id'])
 
+        if 'title' in request.artgs:
+            query = query.filter_by(func.lower(title)=func.lower(request.args['title']))
+
         if 'creatorDisplayName' in request.args:
             query = query.filter(func.lower(User.displayName) == func.lower(request.args['creatorDisplayName']))
 
